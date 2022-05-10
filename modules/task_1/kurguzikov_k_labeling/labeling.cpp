9fc28058acb9702509846ad595cdb84b8ae67229
Kirill <kurguzikov.k@mail.ru>
2022-05-10 23:34:03 +0300

    fix

--- modules/task_1/kurguzikov_k_labeling/labeling.cpp
+++ modules/task_1/kurguzikov_k_labeling/labeling.cpp
@@ -93,7 +93,7 @@ inline std::vector<uint32_t> renumberComponents(std::vector<uint32_t> image, int
     }
     for (int i = 0; i < size; ++i) {
         if (image[i] != 0) {
-            image[i] = result[std::distance(source.begin(), std::find(source.begin(), source.end(), image[i])];
+            image[i] = result[std::distance(source.begin(), std::find(source.begin(), source.end(), image[i]];
         }
     }
     return image;
