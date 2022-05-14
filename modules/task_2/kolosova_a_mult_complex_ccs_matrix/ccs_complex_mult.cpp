45a04214c2af3ee0a35ea6e6f581ad2b9af3fd85
xiibug <alenak58@gmail.com>
2022-05-14 23:29:04 +0300

    Minor efficiency improvements

--- modules/task_2/kolosova_a_mult_complex_ccs_matrix/ccs_complex_mult.cpp
+++ modules/task_2/kolosova_a_mult_complex_ccs_matrix/ccs_complex_mult.cpp
@@ -35,9 +35,9 @@ CCS_matrix::CCS_matrix(std::vector<std::vector<std::complex<double>>> m) {
     row_n = m.size();
     col_n = (row_n) ? m[0].size() : 0;
     int ptr = 0;
-    for (int j = 0; j < static_cast<int>(m[0].size()); j++) {
+    for (int j = 0; j < col_n; j++) {
         column_pointer.push_back(ptr);
-        for (int i = 0; i < static_cast<int>(m.size()); i++) {
+        for (int i = 0; i < row_n; i++) {
             if (m[i][j].real() || m[i][j].imag()) {
                 val.push_back(m[i][j]);
                 rows.push_back(i);
@@ -100,22 +100,32 @@ CCS_matrix multiply(const CCS_matrix& A, const CCS_matrix& B) {
         throw("Incompatible matrices size for multiplication");
     CCS_matrix At = transpose(A);
 
-    std::vector<std::vector<std::complex<double>>> C(A.row_n);
-    for (int i = 0; i < static_cast<int>(C.size()); i++) {
-        C[i] = std::vector<std::complex<double>>(B.col_n, {0.0, 0.0});
-    }
+    std::vector<std::vector<std::complex<double>>> tmpVals(B.col_n);
+    std::vector<std::vector<int>> tmpRows(B.col_n);
 
-    for (int i = 0; i < A.row_n; i++) {
-        if (At.column_pointer[i] == At.column_pointer[i + 1]) continue;
-        for (int j = 0; j < B.col_n; j++) {
-            if (B.column_pointer[j] == B.column_pointer[j + 1]) continue;
-            C[i][j] = scalar_mult(At.val, At.rows, At.column_pointer[i],
+    for (int j = 0; j < B.col_n; j++) {
+        if (B.column_pointer[j] == B.column_pointer[j + 1]) continue;
+        for (int i = 0; i < A.row_n; i++) {
+            if (At.column_pointer[i] == At.column_pointer[i + 1]) continue;
+            std::complex<double> v = scalar_mult(At.val, At.rows, At.column_pointer[i],
                 At.column_pointer[i + 1],
                 B.val, B.rows, B.column_pointer[j], B.column_pointer[j + 1]);
+            if (v != 0.0) {
+                tmpVals[j].push_back(v);
+                tmpRows[j].push_back(i);
+            }
         }
     }
 
-    CCS_matrix res(C);
+    CCS_matrix res(A.row_n, B.col_n);
+    int tmpCol = 0;
+    for (int i = 0; i < res.col_n; i++) {
+        res.column_pointer.push_back(tmpCol);
+        res.val.insert(res.val.end(), tmpVals[i].begin(), tmpVals[i].end());
+        res.rows.insert(res.rows.end(), tmpRows[i].begin(), tmpRows[i].end());
+        tmpCol += tmpVals[i].size();
+    }
+    res.column_pointer.push_back(tmpCol);
     return res;
 }
 
@@ -124,23 +134,33 @@ CCS_matrix multiply_omp(const CCS_matrix& A, const CCS_matrix& B) {
         throw("Incompatible matrices size for multiplication");
     CCS_matrix At = transpose_omp(A);
 
-    std::vector<std::vector<std::complex<double>>> C(A.row_n);
-    for (int i = 0; i < static_cast<int>(C.size()); i++) {
-        C[i] = std::vector<std::complex<double>>(B.col_n, { 0.0, 0.0 });
-    }
+    std::vector<std::vector<std::complex<double>>> tmpVals(B.col_n);
+    std::vector<std::vector<int>> tmpRows(B.col_n);
 
 #pragma omp parallel for
-    for (int i = 0; i < A.row_n; i++) {
-        if (At.column_pointer[i] == At.column_pointer[i + 1]) continue;
-        for (int j = 0; j < B.col_n; j++) {
-            if (B.column_pointer[j] == B.column_pointer[j + 1]) continue;
-            C[i][j] = scalar_mult(At.val, At.rows, At.column_pointer[i],
-                At.column_pointer[i + 1],
-                B.val, B.rows, B.column_pointer[j], B.column_pointer[j + 1]);
-        }
-    }
+	for (int j = 0; j < B.col_n; j++) {
+		if (B.column_pointer[j] == B.column_pointer[j + 1]) continue;
+        for (int i = 0; i < A.row_n; i++) {
+			if (At.column_pointer[i] == At.column_pointer[i + 1]) continue;
+            std::complex<double> v = scalar_mult(At.val, At.rows, At.column_pointer[i],
+				At.column_pointer[i + 1],
+				B.val, B.rows, B.column_pointer[j], B.column_pointer[j + 1]);
+            if (v != 0.0) {
+                tmpVals[j].push_back(v);
+                tmpRows[j].push_back(i);
+            }
+		}
+	}
 
-    CCS_matrix res(C);
+    CCS_matrix res(A.row_n, B.col_n);
+    int tmpCol = 0;
+    for (int i = 0; i < res.col_n; i++) {
+        res.column_pointer.push_back(tmpCol);
+        res.val.insert(res.val.end(), tmpVals[i].begin(), tmpVals[i].end());
+        res.rows.insert(res.rows.end(), tmpRows[i].begin(), tmpRows[i].end());
+        tmpCol += tmpVals[i].size();
+    }
+    res.column_pointer.push_back(tmpCol);
     return res;
 }
 
