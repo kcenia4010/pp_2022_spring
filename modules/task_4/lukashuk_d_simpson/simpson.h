5704de9cfd93e7254178f9e8a8de7ad562b3385d
DianaLed <61021228+DianaLed@users.noreply.github.com>
2022-05-26 17:17:51 +0300

    style commit

--- modules/task_4/lukashuk_d_simpson/simpson.h
+++ modules/task_4/lukashuk_d_simpson/simpson.h
@@ -3,19 +3,19 @@
 #define MODULES_TASK_4_LUKASHUK_D_SIMPSON_SIMPSON_H_
 
 #include <algorithm>
+#include <chrono>  // NOLINT [build/c++11]
 #include <cmath>
+#include <thread>  // NOLINT [build/c++11]
 #include <utility>
 #include <vector>
-#include <chrono>  // NOLINT [build/c++11]
-#include <thread>  // NOLINT [build/c++11]
 
 typedef double (*FUNC_T)(const std::vector<double>&);
 typedef std::vector<std::pair<double, double>> BORDER_T;
 
 double simpson(const FUNC_T& f, const BORDER_T& borders,
-    const std::vector<int>& steps);
+               const std::vector<int>& steps);
 
 double simpson_par(const FUNC_T& f, const BORDER_T& borders,
-    const std::vector<int>& steps);
+                   const std::vector<int>& steps);
 
 #endif  // MODULES_TASK_4_LUKASHUK_D_SIMPSON_SIMPSON_H_
